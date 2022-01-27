d99eecc04bb75bbd1334a85805862c113bacf757
Dev-Elie <eochieng9448@gmail.com>
2022-01-27 18:45:13 +0300

    Modified Config

--- config.py
+++ config.py
@@ -26,5 +26,5 @@ class DevelopmentConfig(Config):
     SECRET_KEY = environ.get("SECRET_KEY")
 
 class TestingConfig(Config):
-    SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + path.join(basedir, "test.sqlite")
     TESTING = True
